11f9f5d6e3a82928fdd3737c42674110ed54d03f
MICHEALSHOE2000 <91520503+MICHEALSHOE2000@users.noreply.github.com>
2025-07-16 10:05:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Countdown Timer
 function countdown() {
-  const offerDate = new Date("2025-06-30T23:59:59");
+  const offerDate = new Date("2025-07-30T23:59:59");
   const now = new Date();
   const diff = offerDate - now;
 
